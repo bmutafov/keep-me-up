cd61713a32436014446085784d4e6c46d52388eb
Boris Mutafov <bmutafov@gmail.com>
2022-02-09 16:49:43 +0200

    Implement interval, offset and quiet arguments

--- README.md
+++ README.md
@@ -2,11 +2,13 @@
 
 A simple quick program that keeps your PC awake by moving your mouse by 1px diagonally every 30 seconds. It has no interface as is intended to be as simple as possible.
 
-# Run via npx
+## Run via npx
 
-To run the awake, just open a new terminal and run `npx simple-keep-pc-awake`. This will start the script. When you want to stop it just exit with `Ctrl+C`.
+To run the awake, just open a new terminal and run
+`npx simple-keep-pc-awake`
+This will start the script. When you want to stop it just exit with `Ctrl+C`.
 
-# Running via node
+## Running via node
 
 To keep with node on your pc you have to perform the following steps:
 
@@ -19,6 +21,26 @@ This will start the awake process and will run until you stop with with `Ctrl+C`
 
 You can create .bat scripts or bash aliases to quickly start and stop the process when needed.
 
-# Running via executable
+## Running via executable
 
 To run via built executables, go to [the releases tab in the repo](https://github.com/mutafow/simple-keep-pc-awake/releases) and download the latest version of the binaries for your machine. Then simply run it when you need to keep your PC awake and close it when you don't!
+
+## Options
+
+When running via node or NPX you can set some options:
+
+```
+npx simple-keep-pc-awake [options]
+
+  Options:
+
+    --quiet               quiet mode, does not output any logs (default: false)
+    --offset              by how many pixels should the mouse move (default: 1)
+    --interval            how much time should pass before mouse is moved again, in seconds (default: 30s)
+
+  Examples:
+
+    npx simple-keep-pc-awake --quiet --offset 5             moves the cursor by 5px each 30 seconds, in quiet mode
+    npx simple-keep-pc-awake --offset 5 --interval 100      moves the cursor by 5px each 100 seconds
+    npx simple-keep-pc-awake                                default behavior, moves the cursor by 1px every 30 seconds
+```
--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 #!/usr/bin/env node
 //@ts-check
 
+// Import nut-js which handles the mouse-moving functionalities
 const { mouse, Point } = require("@nut-tree/nut-js");
 
+// Read the command line arguments
+const args = process.argv.slice(2);
+
+const DEFAULTS = {
+  OFFSET_PX: 1,
+  INTERVAL_SEC: 30,
+};
+
+//#region time-helpers
 /**
  * Representing the digits in a double digit format (0 to 00, 8 to 08, 12 to 12, etc.);
  * @param {number} digit
@@ -27,22 +37,93 @@ const now = () => {
 
   return `${hours}:${minutes}:${seconds}`;
 };
+//#endregion
+
+//#region args-parser-helpers
+
+/**
+ * Extracts argument from the process args with the key provided
+ * @param {string} param Key of the extracted value
+ * @param {number} defaultValue Default value to be used if no such value is found.
+ * @returns
+ */
+const getArgParam = (param, defaultValue) => {
+  // Find the arg index which corresponds to the param provided
+  const paramTagIndex = args.findIndex((arg) => arg === param);
+  if (paramTagIndex < 0) return defaultValue;
+
+  // Get the next element in the array, so we know the value of the param
+  const paramValueIndex = paramTagIndex + 1;
+
+  // If there is such value after param tag
+  if (args[paramValueIndex]) {
+    // Try to parse it to float
+    const paramValueAsFloat = parseFloat(args[paramValueIndex]);
+
+    // If it is not a valid number, return the default value
+    if (isNaN(paramValueAsFloat)) {
+      return defaultValue;
+    }
+    // Else return the user-configured value
+    else {
+      return paramValueAsFloat;
+    }
+  }
+
+  // Default return, if no arg after --offset is provided
+  return defaultValue;
+};
+
+/**
+ * Extracts the offset value from the --offset argument
+ * @returns {number} Amount of pixels to move the cursor by. User defined or default.
+ */
+const getMoveOffset = () => {
+  return getArgParam("--offset", DEFAULTS.OFFSET_PX);
+};
+
+/**
+ * Extracts the interval value from the --interval argument
+ * @returns {number} Amount of MS to wait before moving the cursor again. User defined or default.
+ */
+const getMoveInterval = () => {
+  return getArgParam("--interval", DEFAULTS.INTERVAL_SEC) * 1_000;
+};
+
+/**
+ * Searches the args for --quiet param and if found should return true to surpass console.logs
+ */
+const isQuietMode = args.some((arg) => arg === "--quiet");
+//#endregion
+
+/**
+ * console.logs the message if quiet mode is off
+ * @param {string} message
+ */
+const log = (message) => (!isQuietMode ? console.log(message) : null);
 
 /**
  * Moves the mouse cursor by X amount of pixels
  * @param {number} offset How many pixels should the mouse be moved by
+ * @param {number} interval How many milliseconds to wait before moving the mouse again
  */
-const mouseMover = async (offset) => {
-  console.log(`\x1b[33m${now()}\x1b[0m Moving mouse cursor by ${offset} pixels`);
+const mouseMover = async (offset, interval) => {
+  log(`\x1b[33m${now()}\x1b[0m Moved mouse cursor by ${offset} pixels`);
 
   const prevPosition = await mouse.getPosition();
   const coordinates = new Point(prevPosition.x + offset, prevPosition.y + offset);
   await mouse.setPosition(coordinates);
 
   setTimeout(() => {
-    mouseMover(offset * -1);
-  }, 30_000);
+    mouseMover(offset * -1, interval);
+  }, interval);
 };
 
-console.log("\x1b[32mStarting mouse mover script...\x1b[0m");
-mouseMover(1);
+const moveOffset = getMoveOffset();
+const moveInterval = getMoveInterval();
+
+log("\x1b[32m 🚀 Started simple-keep-pc-awake...\x1b[0m");
+log(`\x1b[34m INFO \x1b[0m Moving mouse cursor by ${moveOffset} pixels`);
+log(`\x1b[34m INFO \x1b[0m Moving mouse cursor every ${moveInterval / 1_000} seconds`);
+
+mouseMover(moveOffset, moveInterval);
